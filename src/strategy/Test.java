46dce1f1fd414913c92e414b7f4fe4a7f9a5755b
DOUHIChaimae <c.douhi@etu.enset-media.ac.ma>
2023-10-13 19:57:49 +0100

    refactoring

--- src/strategy/Test.java
+++ src/strategy/Test.java
@@ -4,12 +4,13 @@ import strategy.entities.Employe;
 import strategy.services.IGRCalculator;
 import strategy.services.impl.MarocIGRCalculator;
 
-public class Main {
+public class Test {
     public static void main(String[] args) {
         IGRCalculator igrMarocCalculator = new MarocIGRCalculator();
         Employe employe = new Employe("emp1", 10000);
         employe.setIgrCalculator(igrMarocCalculator);
         float igr = employe.calculerIGR();
-        employe.calculateSalaireNetMensuel(employe.getSalaireBrutMensuel(), igr);
+        System.out.println(igr);
+        System.out.println(employe.calculateSalaireNetMensuel(employe.getSalaireBrutMensuel(), igr));
     }
 }

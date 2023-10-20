d1326ed5048d34eeefb14e66a1aa14aba0e4d537
DOUHIChaimae <c.douhi@etu.enset-media.ac.ma>
2023-10-20 21:40:09 +0100

    refactoring

--- src/td1/exercice1/decorator/ComponentImpl.java
+++ src/td1/exercice1/decorator/ComponentImpl.java
@@ -3,6 +3,6 @@ package td1.exercice1.decorator;
 public class ComponentImpl implements Component {
     @Override
     public void traitement() {
-        System.out.println("=========== processing =============");
+        System.out.println("=========== processing ... ============");
     }
 }

38be540bd14d97b3aca75be97994e8069d4fa10f
DOUHIChaimae <c.douhi@etu.enset-media.ac.ma>
2023-10-20 20:11:58 +0100

    add some comments

--- src/td1/exercice1/templatemethod/TemplateClass.java
+++ src/td1/exercice1/templatemethod/TemplateClass.java
@@ -1,5 +1,10 @@
 package td1.exercice1.templatemethod;
 
+/**
+ * Template Method is a behavioral design pattern that defines the skeleton of an algorithm in the superclass
+ * but lets subclasses override specific steps of the algorithm without changing its structure.
+ */
+
 public abstract class TemplateClass {
     public double getSum(int a, int b) {
         double comp = 0;

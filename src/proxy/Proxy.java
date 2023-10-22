36616c7a5348548d1f443594807f1d2ef9509999
DOUHIChaimae <c.douhi@etu.enset-media.ac.ma>
2023-10-22 18:51:45 +0100

    proxy

--- src/proxy/Proxy.java
+++ src/proxy/Proxy.java
@@ -1,4 +1,19 @@
 package proxy;
 
-public class Proxy {
+import java.util.Random;
+
+public class Proxy implements Standard {
+    private StandardImpl1 target = new StandardImpl1();
+
+    //Control access to the object
+    @Override
+    public void process() {
+        System.out.println("Security context verification...");
+        boolean b = new Random().nextBoolean();
+        if (b) {
+            target.process();
+        } else {
+            throw new RuntimeException("Forbidden 403");
+        }
+    }
 }

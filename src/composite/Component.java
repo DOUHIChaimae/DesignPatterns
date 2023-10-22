acb5a1e2bb5b12300ecf46a964d2c23d6d4fff91
DOUHIChaimae <c.douhi@etu.enset-media.ac.ma>
2023-10-22 12:09:17 +0100

    File class

--- src/composite/Component.java
+++ src/composite/Component.java
@@ -1,8 +1,20 @@
 package composite;
 
 public abstract class Component {
-    private String name;
-    private int level;
+    protected String name;
+    protected int level;
+
+    public Component(String name) {
+        this.name = name;
+    }
 
     public abstract void show();
+
+    public String tab() {
+        String tab = "";
+        for (int i = 0; i < level; i++) {
+            tab += "\t";
+        }
+        return tab;
+    }
 }

4219a48e96044df45c5ebf1a803e4e2d701f9fc9
DOUHIChaimae <c.douhi@etu.enset-media.ac.ma>
2023-10-22 09:07:43 +0100

    Couloi impl

--- src/td1/exercice1/observer/Couloir.java
+++ src/td1/exercice1/observer/Couloir.java
@@ -4,9 +4,27 @@ public class Couloir implements Observer {
     @Override
     public void update(Observable observable) {
         int score = ((Joueur) observable).getScore();
-        double result = Math.pow(score, score) + 19;
-        System.out.println("** Couloir **");
-        System.out.println("Updated score: " + score);
-        System.out.println("Result: " + result);
+        System.out.println("Player's score has changed to: " + score);
+        handleScore(score);
+    }
+
+    private void handleScore(int score) {
+        if (score > 75) {
+            displayHighScoreMessage();
+        } else {
+            displayNormalScoreMessage();
+        }
+    }
+
+    private void displayHighScoreMessage() {
+        displayMessage("High score achieved! Lights are flashing.");
+    }
+
+    private void displayNormalScoreMessage() {
+        displayMessage("Normal score. Lights remain steady.");
+    }
+
+    private void displayMessage(String message) {
+        System.out.println(message);
     }
 }

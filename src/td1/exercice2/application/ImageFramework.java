596775cc32e4b165763aa20dd8dd88c45a2f525f
DOUHIChaimae <c.douhi@etu.enset-media.ac.ma>
2023-10-26 22:21:25 +0100

    refactoring

--- src/td1/exercice2/application/ImageFramework.java
+++ src/td1/exercice2/application/ImageFramework.java
@@ -1,4 +1,11 @@
 package td1.exercice2.application;
 
+import td1.exercice2.compression.templateMethod.ImageCompressionAlgorithm;
+import td1.exercice2.filtrage.strategy.services.FilterStrategy;
+
 public class ImageFramework {
+    private FilterStrategy filterStrategy;
+    private ImageCompressionAlgorithm imageCompressionAlgorithm;
+
+
 }
--- src/td1/exercice2/compression/templateMethod/ImageCompressionAlgorithm.java
+++ src/td1/exercice2/compression/templateMethod/ImageCompressionAlgorithm.java
@@ -1,4 +1,5 @@
 package td1.exercice2.compression.templateMethod;
 
 public abstract class ImageCompressionAlgorithm {
+
 }

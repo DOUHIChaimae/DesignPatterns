733254b4cc9ff6937b52f6e408f8bc1e093da1f5
DOUHIChaimae <c.douhi@etu.enset-media.ac.ma>
2023-10-26 22:42:16 +0100

    CustomIMageCompressionAlgorithm1

--- src/td1/exercice2/compression/templateMethod/ImageCompressionAlgorithm.java
+++ src/td1/exercice2/compression/templateMethod/ImageCompressionAlgorithm.java
@@ -1,5 +1,20 @@
 package td1.exercice2.compression.templateMethod;
 
 public abstract class ImageCompressionAlgorithm {
+    public int[] compress(int[] data) {
+        int[] preprocessedData = preprocess(data);
+
+        int[] compressedData = performCompression(preprocessedData);
+
+        int[] postprocessedData = postprocess(compressedData);
+
+        return postprocessedData;
+    }
+
+    protected abstract int[] preprocess(int[] data);
+
+    protected abstract int[] performCompression(int[] data);
+
+    protected abstract int[] postprocess(int[] data);
 
 }
